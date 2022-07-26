7bd5dfa1e3335f74d3441ce847ac6adf0d00a090
P9iman <Paiman.Karimy@haw-hamburg.de>
2022-07-27 00:12:15 +0200

    Fxml File created with ui components

--- DiceUI.java
+++ DiceUI.java
@@ -1,6 +1,17 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Pane;
+
 class DiceUI
 {
-    
+    private Button _rollBtn; 
+    private Button _editSidesBtn;
+    private Pane _mainPane; 
 
+    public DiceUI() {
+        
+
+
+    }
 
 }
